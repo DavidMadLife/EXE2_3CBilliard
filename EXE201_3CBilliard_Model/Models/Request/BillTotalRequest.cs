70b7b2998751ff165de3611c364c1da50b9356a7
DLuong <luongpdse162038@fpt.edu.vn>
2024-06-04 05:33:10 +0700

    fix booking, fix email for bida club, create api price

---
+++ EXE201_3CBilliard_Model/Models/Request/BillTotalRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE201_3CBilliard_Model.Models.Request
+{
+    public class BillTotalRequest
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
