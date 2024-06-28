17d1123dfd3fb14c66cc31d76981d00eb68ef48c
luongpd-dev <135132004+luongpd-dev@users.noreply.github.com>
2024-06-29 00:29:48 +0700

    add img for product

--- EXE201_3CBilliard_Repository/obj/Debug/net6.0/EXE201_3CBilliard_Repository.AssemblyInfo.cs
+++ EXE201_3CBilliard_Repository/obj/Debug/net6.0/EXE201_3CBilliard_Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EXE201_3CBilliard_Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5770eb77d6e59bdb512bff9dd930424c3b5fd3ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EXE201_3CBilliard_Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EXE201_3CBilliard_Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
